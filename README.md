feb7600b905e77b549f3a88df0f308e96fe4a374
Raúl Armas <raul.a.armas@gmail.com>
2020-12-23 00:52:43 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # micumplimiento
+Este proyecto está basado en una tarea que tenía que hacer todas las semanas en uno de mis antigüos trabajo: calcular mi cumplimiento de venta en base a una meta.
+Si obtenía 100% o más de cumplimiento tenía que pintar un círculo con mi nombre de color amarillo.
+Si obtenía entre 90 y menos de 100 (%) tenía que pintarlo de color negro.
+Pero si era menos que eso, entonces tenía que pintarlo de rojo, y además estaba en graves problemas.
+![alt text](https://i.ibb.co/bssCZmC/Screenshot-20201223-005129.png)
---
+++ main.js
@@ -0,0 +1,26 @@
+function calculate() {
+    var goalBox= document.getElementById("goal-box")
+    var fulfilledBox = document.getElementById("fulfilled-box")
+    var fulfilledPercBox = document.getElementById("fulfilled-perc-box")
+    var missingBox = document.getElementById("missing-box")
+    var indicator = document.getElementById("indicator-color")
+
+    goalBox = Number(goalBox.value)
+    fulfilledBox = Number(fulfilledBox.value)
+
+    var missing = (fulfilledBox > goalBox) ? 0 : goalBox - fulfilledBox
+    missing = missing.toFixed(2)
+    missingBox.innerHTML = missing
+    
+    var fulfilledPerc = fulfilledBox * 100 / goalBox
+    fulfilledPerc = fulfilledPerc.toFixed(2)
+    fulfilledPercBox.innerHTML = fulfilledPerc + "%"
+
+    if (fulfilledPerc >= 100) {
+        indicator.style.backgroundColor = "yellow"
+    } else if (fulfilledPerc >= 90) {
+        indicator.style.backgroundColor = "black"
+    } else {
+        indicator.style.backgroundColor = "red"
+    }
+}
